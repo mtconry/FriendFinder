db729a84f3f10d9ea18f27b00ffa8e0cc61bb3b3
Mike Conry <mikeconry@Mikes-MacBook-Pro.local>
2019-02-25 15:33:38 -0500

    all files

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.listen(PORT, function() {
 });
 
 // Requires and sets the HTML routes in this file
-require('./app/routing/htmlRoutes.js')(app);
+require('./app/routing/htmlRoute.js')(app);
 
 // Requires and sets the API routes in this file
 require('./app/routing/apiRoutes.js')(app);
